ac56321c2f2825106137598b272741a38646a840
Huong14 <thienhuongbluesky14@gmail.com>
2026-06-14 18:48:50 +0900

    upload

---
+++ src/Baitap4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Baitap4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập địa chỉ email: ");
+        String email = scanner.nextLine().trim();
+
+        String emailRegex = "^[a-zA-Z0-9._]+@[a-zA-Z0-9]+(\\.[a-zA-Z]{2,6})+$";
+
+        if (email.matches(emailRegex)) {
+            System.out.println("Email hợp lệ");
+        } else {
+            System.out.println("Email không hợp lệ");
+        }
+
+        scanner.close();
+    }
+}
